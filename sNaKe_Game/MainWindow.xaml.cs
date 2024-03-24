54dda2d59f681e0c478d7ed26a5e0d68256b535d
Mullins182 <Mullins182@t-online.de>
2024-03-24 13:25:23 +0100

    building Segment movement logic.

--- sNaKe_Game/MainWindow.xaml.cs
+++ sNaKe_Game/MainWindow.xaml.cs
@@ -1,30 +1,110 @@
-﻿using System.Drawing;
-using System.Text;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
-namespace sNaKe_Game
+namespace SnakeGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private const int SnakeSizeH = 30;
+        private const int SnakeSizeW = 50;
+        private const int SnakeSpeed = 100;
+        private Point currentPosition = new(1000, 350);
+        private DispatcherTimer timer = new();
+        private Direction direction = Direction.Left;
+        private UIElement? snakeFood = null;
+        private SolidColorBrush foodBrush = Brushes.Red;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.KeyDown += new KeyEventHandler(OnButtonKeyDown);
+            paintSnake(currentPosition);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, SnakeSpeed);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void paintSnake(Point currentPosition)
+        {
+            Ellipse Segment = new();
+            Segment.Fill = Brushes.Red;
+            Segment.Width = SnakeSizeW;
+            Segment.Height = SnakeSizeH;
+            //currentPosition.X = 1000;
+            //currentPosition.Y = 350;
+            Canvas.SetTop(Segment, currentPosition.Y);
+            Canvas.SetLeft(Segment, currentPosition.X);
+            int count = play_area.Children.Count;
+            play_area.Children.Add(Segment);
+            currentPosition = new Point(currentPosition.X, currentPosition.Y);
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            // Update the position of the snake and repaint it
+
+            if (direction == Direction.Left)
+            {
+                currentPosition.X -= 5;
+                Canvas.SetLeft(play_area.Children[0], currentPosition.X);
+            }
+            else if (direction == Direction.Right) 
+            {
+                currentPosition.X += 5;
+                Canvas.SetLeft(play_area.Children[0], currentPosition.X);
+            }
+            else if (direction == Direction.Up) 
+            {
+                currentPosition.Y -= 5;
+                Canvas.SetTop(play_area.Children[0], currentPosition.Y);
+            }
+            else if (direction == Direction.Down) 
+            {
+                currentPosition.Y += 5;
+                Canvas.SetTop(play_area.Children[0], currentPosition.Y);
+            }
+        }
+
+        private void OnButtonKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    if (direction != Direction.Down)
+                        direction = Direction.Up;
+                    break;
+                case Key.Down:
+                    if (direction != Direction.Up)
+                        direction = Direction.Down;
+                    break;
+                case Key.Left:
+                    if (direction != Direction.Right)
+                        direction = Direction.Left;
+                    break;
+                case Key.Right:
+                    if (direction != Direction.Left)
+                        direction = Direction.Right;
+                    break;
+            }
         }
 
         private void quit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
+    }
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
     }
 }
