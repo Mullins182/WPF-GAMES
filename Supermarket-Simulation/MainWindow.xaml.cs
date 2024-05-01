5c296700eb7cf89825f6b5d7cec381208b612172
Mullins182 <Mullins182@t-online.de>
2024-05-01 12:58:44 +0200

    menu + doubleanimation for menu implemented.

--- Supermarket-Simulation/MainWindow.xaml.cs
+++ Supermarket-Simulation/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -16,9 +17,34 @@ namespace Supermarket_Simulation
     /// </summary>
     public partial class MainWindow : Window
     {
+        DoubleAnimation hide_menu = new();
+        DoubleAnimation show_menu = new();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            hide_menu.Duration  = TimeSpan.FromSeconds(0.5);
+            hide_menu.From      = 1.0;
+            hide_menu.To        = 0.0;
+            show_menu.Duration  = TimeSpan.FromSeconds(0.5);
+            show_menu.From      = 0.0;
+            show_menu.To        = 1.0;
+        }
+
+        private void quit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menu_grid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            menu_grid.BeginAnimation(OpacityProperty, show_menu);
+        }
+
+        private void menu_grid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            menu_grid.BeginAnimation(OpacityProperty, hide_menu);
         }
     }
 }
