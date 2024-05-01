38aa6e3741b431bd840e995ab54396f9f42fbe7d
Mullins182 <Mullins182@t-online.de>
2024-05-01 17:48:53 +0200

    debug Gamestatus visualizing ellipse position.

--- Supermarket-Simulation/MainWindow.xaml.cs
+++ Supermarket-Simulation/MainWindow.xaml.cs
@@ -20,19 +20,34 @@ namespace Supermarket_Simulation
     {
         private readonly DoubleAnimation hide_menu      = new();
         private readonly DoubleAnimation show_menu      = new();
+        private readonly DoubleAnimation gameActivity   = new();
 
         private readonly DispatcherTimer gameroutine    = new();
 
-        private int loopCounter = 0;
+        private bool gameIsActive                       = false;
 
-        private void Set_MenuAnimationProperties()
+        private void Set_DoubleAnimationProperties()
         {
-            hide_menu.Duration  = TimeSpan.FromSeconds(0.5);
-            hide_menu.From      = 1.0;
-            hide_menu.To        = 0.0;
-            show_menu.Duration  = TimeSpan.FromSeconds(0.5);
-            show_menu.From      = 0.0;
-            show_menu.To        = 1.0;
+            hide_menu.Duration          = TimeSpan.FromSeconds(0.5);
+            hide_menu.From              = 1.0;
+            hide_menu.To                = 0.0;
+            show_menu.Duration          = TimeSpan.FromSeconds(0.5);
+            show_menu.From              = 0.0;
+            show_menu.To                = 1.0;
+            gameActivity.Duration       = TimeSpan.FromSeconds(0.5);
+            gameActivity.From           = 0.0;
+            gameActivity.To             = 1.0;
+            gameActivity.AutoReverse    = true;
+            gameActivity.Completed      += GameStatusAnimLoop;
+        }
+
+        private void GameStatusAnimLoop(object? sender, EventArgs e)
+        {
+            if (gameIsActive)
+            {
+                GameStatus.BeginAnimation(OpacityProperty, gameActivity);
+            }
+            gameIsActive = false;
         }
 
         private void Set_DispatcherTimerProperties()
@@ -41,22 +56,29 @@ namespace Supermarket_Simulation
             gameroutine.Tick        += Gameroutine;
         }
 
+        private void Set_GameStatusEllipsePos()
+        {
+            Canvas.SetTop(GameStatus, 0.0);
+            Canvas.SetLeft(GameStatus, this.Width - GameStatus.Width);
+        }
+
         public MainWindow()
         {
             InitializeComponent();
 
-            Set_MenuAnimationProperties();
+            Set_DoubleAnimationProperties();
             Set_DispatcherTimerProperties();
+            Set_GameStatusEllipsePos();
+
         }
 
         // GAMEROUTINE SECTION
         private void Gameroutine(object? sender, EventArgs e)
         {
-            loopCounter += 1;
-            GameTime.Content = loopCounter;
+            gameIsActive = true;
         }
 
-            // GAMEROUTINE SECTION END
+                                                                // GAMEROUTINE SECTION END
 
         // UI-Controls-Events
         private void Quit_Click(object sender, RoutedEventArgs e)
@@ -79,15 +101,14 @@ namespace Supermarket_Simulation
             if (gameroutine.IsEnabled)
             {
                 gameroutine.Stop();
+                gameIsActive = false;
             }
             else
             {
-                GameTime.Visibility = Visibility.Visible;
-                GameTime.Content = "";
                 gameroutine.Start();
+                GameStatus.BeginAnimation(OpacityProperty, gameActivity);
             }
         }
-
-        // UI-Controls-Events END
+                                                            // UI-Controls-Events END
     }
 }
