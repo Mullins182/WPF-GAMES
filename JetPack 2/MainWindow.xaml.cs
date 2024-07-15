7c34ae35dcb948548b7e1b422ece1d924da885a9
Mullins182 <Mullins182@t-online.de>
2024-07-15 22:31:49 +0200

    DispatcherTimer implemented ... KeyUp and KeyDown Event integrated and Jump function for player building started. Version update to v0.3

--- JetPack 2/MainWindow.xaml.cs
+++ JetPack 2/MainWindow.xaml.cs
@@ -9,31 +9,51 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace JetPack_2
 {
     public partial class MainWindow : Window
     {
-        Player player = new();
+        private readonly DispatcherTimer GameRoutine = new();
+
+        private Player player   = new();
+
+        private bool moveLeft   = false;
+        private bool moveRight  = false;
+        private bool jump       = false;
 
         public MainWindow()
         {
             InitializeComponent();
-            GameInitializing();
+            Game_ini();
         }
 
-        private void GameInitializing()
+        private void Game_ini()
         {
+            GameRoutine.Interval = TimeSpan.FromMilliseconds(30);
+            GameRoutine.Tick += GameRoutine_Tick;
+
             player.SetPlayerProps();
             SetPlayerPos();
             GenerateLevel1();
+
+            GameRoutine.Start();
+        }
+
+        private void GameRoutine_Tick(object? sender, EventArgs e)
+        {
+            player.PosY = jump ? player.PosY -= 5 : player.PosY >= 880 ? player.PosY += 0 : player.PosY += 5;
+            Canvas.SetTop(player.PlayerModel, player.PosY);
         }
 
         private void GenerateLevel1()
         {
             Platforms Ground = new(0, 940, Brushes.Green, 20, 1500);
             Rectangle ground = Ground.GeneratePlatform();
+
             GameCanvas.Children.Add(ground);
+
             Canvas.SetTop(ground, Ground.PosY);
             Canvas.SetLeft(ground, Ground.PosX);
         }
@@ -41,8 +61,19 @@ namespace JetPack_2
         private void SetPlayerPos()
         {
             GameCanvas.Children.Add(player.PlayerModel);
+
             Canvas.SetTop(player.PlayerModel, player.PosY);
             Canvas.SetLeft(player.PlayerModel, player.PosX);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            jump = e.Key == Key.Space ? true : false;
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            jump = e.Key == Key.Space ? false : jump;
         }
     }
 }
